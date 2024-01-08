b4f33e6cda6c0f908b7561a3b03a8aeb3838697a
nnalaj <jalannlittle923@gmail.com>
2024-01-08 02:55:15 -0500

    updates

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './main.scss';
-import { Meteor } from 'meteor-node-stubs'
+//import { Meteor } from 'meteor-node-stubs'
 import {RouterProvider} from 'react-router-dom';
 import reactDom from 'react-dom/client';
 import {Router} from './imports/api/Router.jsx';
@@ -10,17 +10,19 @@ import {Router} from './imports/api/Router.jsx';
 //TODO: implement error landing page (done)
 //TODO: impoment router ( finally done)
 
-Meteor.startup(() => {
+//Meteor.startup(() => {
 
-  reactDom.createRoot(document.getElementById('react-target')).render(   //have react-dom connect to react-target
-  <RouterProvider router = {Router} />  //render the app by taking the router to the app route
-   )
+  // reactDom.createRoot(document.getElementById('react-target')).render(   //have react-dom connect to react-target
+  // <RouterProvider router = {Router} />  //render the app by taking the router to the app route
+  //  )
   
   
-  //  const container = document.getElementById('react-target');
-  // const root = createRoot(container);
+   const container = document.getElementById('react-target');
+  const root = createRoot(container);
   // <BrowserRouter>      
-  //   <App />        
+  reactDom.createRoot(container).render(
+    <App />      
+  )  
   // </BrowserRouter>
 //no longer needed but good to have just in case router falls through (try this when you wake up)
-});
+//});
