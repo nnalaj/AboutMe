8d4ccc7b62ea593af9e21f8029c0662103364a49
nnalaj <jalannlittle923@gmail.com>
2024-01-13 12:12:16 -0500

    more updates

--- src/imports/ui/Home.jsx
+++ src/imports/ui/Home.jsx
@@ -5,8 +5,10 @@ import Nav from "./Nav";
 const Home = () => {
 
     console.log('on home page');
+    
     return(
        <div>
+            <h1>HOME</h1>
              <Nav />   {/*rendered grid in seperate component to save space */}
             <Video /> {/* video background component render last for speed */}
         </div> 
