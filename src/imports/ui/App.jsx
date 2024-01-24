5cfe20952085810062b6de5480256b804b665d9b
nnalaj <jalannlittle923@gmail.com>
2024-01-23 23:18:53 -0500

    finally fixed everything

--- src/imports/ui/App.jsx
+++ src/imports/ui/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Home from "./Home.jsx";
 
 
+
 export const App = () => (
   <div>
     
--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import "./main.scss";
 import { createRoot } from 'react-dom/client';
 import  {App}  from './/imports/ui/App';
+import { HashRouter } from 'react-router-dom';
 
   const container = document.getElementById('react-target');
   const root = createRoot(container);
-  root.render(<App />);
+  root.render(
+  
+    <HashRouter>
+       <App />
+    </HashRouter>
+ 
+  );
 
