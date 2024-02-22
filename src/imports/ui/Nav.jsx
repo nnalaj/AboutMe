d48ebe3d7860c789ae0f8a3d13a25a2da31d0600
nnalaj <jalannlittle923@gmail.com>
2024-02-22 14:26:47 -0500

    adding media queries idea

--- src/imports/ui/Nav.jsx
+++ src/imports/ui/Nav.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import NNALAJ from "./NNALAJ.jsx";
+import Resume from "./Resume.jsx";
 import {Link, Outlet} from "react-router-dom";
 import Installs from "./Installs.jsx"
 
